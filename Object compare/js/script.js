53f128c357db9a9a70c27c3a72701165dfa3bba7
avinashkumarm91 <avinashkumarm91@gmail.com>
2024-05-21 16:36:52 +0530

    Adding

---
+++ Object compare/js/script.js
@@ -0,0 +1,9 @@
+let obj1={name:"Avinash", age:25};
+let obj2={name:"Rahul", age:25};
+
+if(Array.length.obj1 = Array.length.obj2) {
+    console.log("True1");
+}else
+{
+    console.log("False");
+}
